18da9df2265e6dc952d5559633c61dc75b7a4f08
Leandro Candido <leandrocandido638@gmail.com>
2024-11-13 16:53:44 -0400

    Codigos de Ordenação

---
+++ Aula_11/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Ordenação{
+    static void Ordenar(int[] numeros){
+        int aux;
+        for( int i = 0; i < numeros.Length; i++){
+            for(int j = 0; j < numeros.Length - 1; j++){
+                if(numeros[j] > numeros[j+1]){
+                    aux = numeros[j];
+                    numeros[j] = numeros[j+1];
+                    numeros[j+1] = aux;
+                }
+            }
+        }
+        MostrarOrdenado(numeros);
+    }
+    static void MostrarOrdenado(int[] arr){
+        Console.WriteLine("\nArray ordenado:"); 
+        for(int i = 0; i < arr.Length; i++){
+            Console.Write(arr[i] + " ");
+        }
+    }
+    static void Main1(string[] args){
+        int[] numeros = {5, 3, 8, 1, 9, 2};
+        Console.WriteLine("Array desordenado:");
+        for(int i = 0; i < numeros.Length; i++){
+            Console.Write(numeros[i] + " ");
+        }
+        Ordenar(numeros);
+    }
+}
